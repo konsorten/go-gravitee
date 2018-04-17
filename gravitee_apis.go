344f0fcf211086f432ea343db1b00ccf657e9ed9
Felix Kollmann <fk@konsorten.de>
2018-04-17 17:32:56 +0200

    Added support for retrieving api details

--- gravitee_apis.go
+++ gravitee_apis.go
@@ -42,6 +42,61 @@ func (ai ApiInfo) String() string {
 	return fmt.Sprintf("%v (%v, %v)", ai.Name, ai.ID, ai.State)
 }
 
+type ApiDetailsEndpoint struct {
+	Name     string `json:"name"`
+	Target   string `json:"target"`
+	Weight   int    `json:"weight"`
+	IsBackup bool   `json:"backup"`
+	Type     string `json:"type"`
+
+	Http struct {
+		ConnectTimeoutMS         int  `json:"connectTimeout"`
+		IdleTimeoutMS            int  `json:"idleTimeout"`
+		ReadTimeoutMS            int  `json:"readTimeout"`
+		KeepAlive                bool `json:"keepAlive"`
+		Pipelining               bool `json:"pipelining"`
+		MaxConcurrentConnections int  `json:"maxConcurrentConnections"`
+		UseCompression           bool `json:"useCompression"`
+		FollowRedirects          bool `json:"followRedirects"`
+	} `json:"http"`
+}
+
+func (ai ApiDetailsEndpoint) String() string {
+	return fmt.Sprintf("%v (%v, %v)", ai.Name, ai.Target, ai.Type)
+}
+
+type ApiDetails struct {
+	ID          string        `json:"id"`
+	Name        string        `json:"name"`
+	Version     string        `json:"version"`
+	Description string        `json:"description"`
+	Visibility  ApiVisibility `json:"visibility"`
+	State       ApiState      `json:"state"`
+	Tags        []string      `json:"tags"`
+	Labels      []string      `json:"labels"`
+	//Paths       []string      `json:"paths"`
+	CreatedAt   int           `json:"created_at"`
+	UpdatedAdd  int           `json:"updated_at"`
+	DeployedAt  int           `json:"deployed_at"`
+	Owner       UserReference `json:"owner"`
+	PictureURL  string        `json:"picture_url"`
+	ContextPath string        `json:"context_path"`
+	Proxy       struct {
+		ContextPath      string               `json:"context_path"`
+		StripContextPath bool                 `json:"strip_context_path"`
+		LoggingMode      string               `json:"loggingMode"`
+		Endpoints        []ApiDetailsEndpoint `json:"endpoints"`
+
+		LoadBalancing struct {
+			Type string `json:"type"`
+		} `json:"load_balancing"`
+	} `json:"proxy"`
+}
+
+func (ai ApiDetails) String() string {
+	return fmt.Sprintf("%v (%v, %v)", ai.Name, ai.ID, ai.State)
+}
+
 // GetAllAPIs retrieves a list of all APIs registered in Gravitee.
 func (s *GraviteeSession) GetAllAPIs() ([]ApiInfo, error) {
 	var result *[]ApiInfo
@@ -53,4 +108,17 @@ func (s *GraviteeSession) GetAllAPIs() ([]ApiInfo, error) {
 	}
 
 	return *result, nil
+}
+
+// GetAPI retrieves details on an API registered in Gravitee.
+func (s *GraviteeSession) GetAPI(id string) (*ApiDetails, error) {
+	var result *ApiDetails
+
+	err := s.getForEntity(&result, "apis", id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
 }
--- gravitee_apis_test.go
+++ gravitee_apis_test.go
@@ -5,14 +5,20 @@ import (
 	"testing"
 )
 
-func TestGetAllAPIs(t *testing.T) {
+func TestGetAPI(t *testing.T) {
 	session := createTestSession(t)
 
 	apis, err := session.GetAllAPIs()
-
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	fmt.Printf("%v\n", apis)
+
+	api, err := session.GetAPI(apis[0].ID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Printf("%v\n", api)
 }
